ddd48c23c5a76c255b3641d95658aae29972ad5a
Victor Dias <victorhugmoraisdias@gmail.com>
2023-08-08 21:09:49 -0300

    adiciona funcionalidade de salvar cursos no banco de dados

--- crud-angular/src/app/courses/course-form/course-form.component.ts
+++ crud-angular/src/app/courses/course-form/course-form.component.ts
@@ -1,31 +1,48 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { CoursesService } from '../services/courses.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-course-form',
   templateUrl: './course-form.component.html',
-  styleUrls: ['./course-form.component.scss']
+  styleUrls: ['./course-form.component.scss'],
 })
 export class CourseFormComponent {
-
   form: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private service: CoursesService,
+    private _snackBar: MatSnackBar,
+    private location: Location
+  ) {
     this.form = this.formBuilder.group({
       name: [null],
-      category: [null]
+      category: [null],
     });
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   onSubmit() {
-
+    this.service.save(this.form.value).subscribe(
+      (result) => this.onSuccess(),
+      (error) => this.onError()
+    );
   }
 
   onCancel() {
-    
+    this.location.back();
+  }
+  
+  private onSuccess() {
+    this._snackBar.open('Curso salvo com sucesso!', '', {duration: 3000});
+    this.location.back();
   }
 
+  private onError() {
+    this._snackBar.open('Erro ao salvar curso!', '', {duration: 3000});
+  }
 }
