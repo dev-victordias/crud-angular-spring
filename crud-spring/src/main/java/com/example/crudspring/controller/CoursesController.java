efd6533d1f503eb5a28c5894a15b778096113bdd
Victor Dias <victorhugmoraisdias@gmail.com>
2023-08-08 21:10:19 -0300

    cria controller de POST para cursos

--- crud-spring/src/main/java/com/example/crudspring/controller/CoursesController.java
+++ crud-spring/src/main/java/com/example/crudspring/controller/CoursesController.java
@@ -2,8 +2,13 @@ package com.example.crudspring.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.crudspring.model.Course;
@@ -15,11 +20,17 @@ import lombok.AllArgsConstructor;
 @RequestMapping("/api/elements")
 @AllArgsConstructor
 public class CoursesController {
-    
+
     private final CourseRepository courseRepository;
 
     @GetMapping
-    public List<Course> list() {
+    public List<Course> listCourses() {
         return courseRepository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course createCourse(@RequestBody Course course) {
+        return courseRepository.save(course);
     }
 }
