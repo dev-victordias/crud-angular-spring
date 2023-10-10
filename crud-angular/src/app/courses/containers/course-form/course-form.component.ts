7e95cad009ece3e4bb5a4cd64fd686eb09a21b9e
Victor Dias <victorhugmoraisdias@gmail.com>
2023-10-09 23:16:50 -0300

    adiciona validacao no front-end

--- crud-angular/src/app/courses/containers/course-form/course-form.component.ts
+++ crud-angular/src/app/courses/containers/course-form/course-form.component.ts
@@ -15,8 +15,8 @@ export class CourseFormComponent implements OnInit {
 
   form = this.formBuilder.group({
     _id: [''],
-    name: [''],
-    category: ['']
+    name: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(20)]],
+    category: ['', [Validators.required]]
   });
 
   constructor(
@@ -56,4 +56,22 @@ export class CourseFormComponent implements OnInit {
   private onError() {
     this._snackBar.open('Erro ao salvar curso!', '', {duration: 3000});
   }
+
+  getErrorMessage(fieldName: string) {
+    	const field = this.form.get(fieldName);
+      if(field?.hasError('required')) {
+        return 'Campo obrigatório!'
+      }
+
+      if(field?.hasError('minlength')) {
+        const requiredLength = field.errors ? field.errors['minlength']['requiredLength'] : 5;
+        return `Tamanho mínimo precisa ser de ${requiredLength} caracteres`;
+      }
+
+      if(field?.hasError('maxlength')) {
+        const requiredLength = field.errors ? field.errors['maxlength']['requiredLength'] : 20;
+        return `Tamanho máximo de ${requiredLength} caracteres excedido`;
+      }
+      return 'Campo inválido!'
+    }
 }
